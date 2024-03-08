b9c789f9287edd64a0bba53bb1b3c730da511db0
Admin <nguyenhungthinha1@gmail.com>
2024-03-08 16:27:14 +0700

    Add first source code file

---
+++ main.cpp
@@ -0,0 +1,405 @@
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <conio.h>
+#include <cctype>
+
+using namespace std;
+
+void FixConsoleWindow() {
+    HWND consoleWindow = GetConsoleWindow();
+    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+    style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+    SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void GotoXY(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+//Constants
+#define MAX_SIZE_SNAKE 10
+#define MAX_SIZE_FOOD 4
+#define MAX_SPEED 3
+
+//Global variables5
+POINT snake[10]; //snake
+POINT food[4]; // food
+int CHAR_LOCK;//used to determine the direction my snake cannot move (At a moment, there is one direction my snake cannot move to)
+int MOVING;//used to determine the direction my snake moves (At a moment, there are three directions my snake can move)
+int SPEED;// Standing for level, the higher the level, the quicker the speed
+int HEIGHT_CONSOLE, WIDTH_CONSOLE;// Width and height of console-screen
+int FOOD_INDEX; // current food-index
+int SIZE_SNAKE; // size of snake, initially maybe 6 units and maximum size may be 10
+int STATE; // State of snake: dead or alive
+
+bool IsValid(int x, int y) {
+    for (int i = 0; i < SIZE_SNAKE; i++) {
+        if (snake[i].x == x && snake[i].y == y) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void GenerateFood() {
+    int x, y;
+    srand(time(NULL));
+    for (int i = 0; i < MAX_SIZE_FOOD; i++) {
+        do {
+            x = rand() % (WIDTH_CONSOLE - 1) + 1;
+            y = rand() % (HEIGHT_CONSOLE - 1) + 1;
+        } while (IsValid(x, y) == false);
+        
+        food[i] = { x,y };
+    }
+}
+
+void ResetData() {
+    //Initialize the global values //width 70 height 20
+    CHAR_LOCK = 'A', MOVING = 'D', SPEED = 1; FOOD_INDEX = 0, WIDTH_CONSOLE = 70, HEIGHT_CONSOLE = 20, SIZE_SNAKE = 4;
+    // Initialize default values for snake
+    snake[0] = { 10, 5 }; snake[1] = { 11, 5 };
+    snake[2] = { 12, 5 }; snake[3] = { 13, 5 };
+    snake[4] = { 14, 5 }; snake[5] = { 15, 5 };
+    GenerateFood();//Create food array
+}
+
+
+/*void DrawBoard(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0) {
+    //Row
+    GotoXY(x, y);
+    for (int i = 0; i < width; i++) {
+        cout << "X";
+    }
+
+    GotoXY(x, y + height - 1);
+    for (int i = 0; i < width; i++) {
+        cout << "X";
+    }
+
+    //Column
+    for (int i = 0; i < height; i++) {
+        GotoXY(x, i);
+        cout << "X";
+        GotoXY(x + width - 1, i);
+        cout << "X";
+    }
+
+    GotoXY(curPosX, curPosY);
+}*/
+
+
+//Sample
+void DrawBoard(int x, int y, int width, int height, char ch, int curPosX = 0, int curPosY = 0) {
+    GotoXY(x, y);cout << ch;
+    for (int i = 1; i < width; i++)cout << ch;
+    cout << ch;
+    GotoXY(x, height + y);cout << ch;
+    for (int i = 1; i < width; i++)cout << ch;
+    cout << ch;
+    for (int i = y + 1; i < height + y; i++){
+    GotoXY(x, i);cout << ch;
+    GotoXY(x + width, i);cout << ch;
+    }
+    GotoXY(curPosX, curPosY);
+}
+
+void StartGame() {
+    system("cls");
+    ResetData(); //Initialize
+    DrawBoard(0, 0, WIDTH_CONSOLE, HEIGHT_CONSOLE, '+'); //Draw game
+    STATE = 1; //Start
+}
+
+void ExitGame(HANDLE t) {
+    system("cls");
+    TerminateThread(t, 0);
+}
+
+void PauseGame(HANDLE t) {
+    SuspendThread(t);
+}
+
+//Function to update global data
+void Eat() {
+    snake[SIZE_SNAKE] = food[FOOD_INDEX];
+    
+    if (FOOD_INDEX == MAX_SIZE_FOOD - 1) { //Eat all the food at this level
+        FOOD_INDEX = 0;
+        
+        if (SPEED == MAX_SPEED) {
+            SPEED = 1;
+            SIZE_SNAKE = 4; //Reset when pass all level
+        }
+        else {
+            SPEED++;
+            SIZE_SNAKE++;
+        }
+        GenerateFood();
+    }
+    else {
+        FOOD_INDEX++;
+        SIZE_SNAKE++;
+    }
+}
+
+//Function to process the dead of snake
+void ProcessDead() {
+    STATE = 0;
+    //GotoXY(snake[SIZE_SNAKE - 1].x, snake[SIZE_SNAKE - 1].y);
+    //cout << "X";
+    GotoXY(0, HEIGHT_CONSOLE + 2); 
+    cout << "Dead, type \'y\' to continue or anykey to exit!";
+}
+
+void DrawSnakeAndFood(char* str) {
+    //DRAW FOOD
+    GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);
+    cout << str;
+
+    //DRAW SNAKE
+    for (int i = 0; i < SIZE_SNAKE; i++) {
+        GotoXY(snake[i].x, snake[i].y);
+        cout << str;
+    }
+}
+
+void DrawFood(char* str) {
+    //DRAW FOOD
+    GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);
+    cout << str;
+}
+
+void DrawSnake(char* str) {
+    for (int i = 0; i < SIZE_SNAKE; i++) {
+        GotoXY(snake[i].x, snake[i].y);
+        cout << str[i];
+    }
+}
+
+bool matchCoordinate(POINT A, POINT B) {
+    return (A.x == B.x && A.y == B.y);
+}
+
+bool hitObstacle(POINT newPoint) {
+    //Border
+    if (newPoint.x == 0 || newPoint.x == WIDTH_CONSOLE || newPoint.y == 0 || newPoint.y == HEIGHT_CONSOLE)
+        return true;
+    
+    //Body
+    for (int i = 0; i < SIZE_SNAKE - 1; i++) {
+        if (matchCoordinate(newPoint, snake[i]))
+            return true;
+    }
+
+    return false;
+}
+
+//Functions for moving the snake
+//snake[0] is the tail, snake[SIZE_SNAKE - 1] is the head
+void MoveRight() {
+    //Touch the "wall"
+    // if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE) {
+    //     ProcessDead();
+    // }
+    POINT temp = snake[SIZE_SNAKE - 1];
+    temp.x += 1;
+    if (hitObstacle(temp)) {
+        //ProcessDead();
+        STATE = 0;
+    }
+
+    //Move
+    else {
+        //Eat
+        if (snake[SIZE_SNAKE - 1].x + 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) {
+            Eat();
+        }
+        //Draw snake when it moves
+        for (int i = 0; i  < SIZE_SNAKE - 1; i++) {
+            snake[i].x = snake[i + 1].x;
+            snake[i].y = snake[i + 1].y;
+        }
+        snake[SIZE_SNAKE - 1].x++;
+    }
+}
+
+void MoveLeft() {
+    //Touch the "wall"
+    // if (snake[SIZE_SNAKE - 1].x - 1 == 0) {
+    //     ProcessDead();
+    // }
+    POINT temp = snake[SIZE_SNAKE - 1];
+    temp.x -= 1;
+    if (hitObstacle(temp)) {
+        //ProcessDead();
+        STATE = 0;
+    }
+
+    //Move
+    else {
+        //Eat
+        if (snake[SIZE_SNAKE - 1].x - 1 == food[FOOD_INDEX].x && snake[SIZE_SNAKE - 1].y == food[FOOD_INDEX].y) {
+            Eat();
+        }
+        //Draw snake when it moves
+        for (int i = 0; i  < SIZE_SNAKE - 1; i++) {
+            snake[i].x = snake[i + 1].x;
+            snake[i].y = snake[i + 1].y;
+        }
+        snake[SIZE_SNAKE - 1].x--;
+    }
+}
+
+void MoveDown() {
+    //Touch the "wall"
+    // if (snake[SIZE_SNAKE - 1].y + 1 == HEIGHT_CONSOLE) {
+    //     ProcessDead();
+    // }
+    POINT temp = snake[SIZE_SNAKE - 1];
+    temp.y += 1;
+    if (hitObstacle(temp)) {
+        //ProcessDead();
+        STATE = 0;
+    }
+
+    //Move
+    else {
+        //Eat
+        if (snake[SIZE_SNAKE - 1].y + 1 == food[FOOD_INDEX].y && snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x) {
+            Eat();
+        }
+        //Draw snake when it moves
+        for (int i = 0; i  < SIZE_SNAKE - 1; i++) {
+            snake[i].x = snake[i + 1].x;
+            snake[i].y = snake[i + 1].y;
+        }
+        snake[SIZE_SNAKE - 1].y++;
+    }
+}
+
+void MoveUp() {
+    //Touch the "wall"
+    // if (snake[SIZE_SNAKE - 1].y - 1 == 0) {
+    //     ProcessDead();
+    // }
+    POINT temp = snake[SIZE_SNAKE - 1];
+    temp.y -= 1;
+    if (hitObstacle(temp)) {
+       //ProcessDead();
+       STATE = 0;
+    }
+
+    //Move
+    else {
+        //Eat
+        if (snake[SIZE_SNAKE - 1].y - 1 == food[FOOD_INDEX].y && snake[SIZE_SNAKE - 1].x == food[FOOD_INDEX].x) {
+            Eat();
+        }
+        //Draw snake when it moves
+        for (int i = 0; i  < SIZE_SNAKE - 1; i++) {
+            snake[i].x = snake[i + 1].x;
+            snake[i].y = snake[i + 1].y;
+        }
+        snake[SIZE_SNAKE - 1].y--;
+    }
+}
+
+//Subfunction for thread
+void ThreadFunc() {
+    while (true) {
+        if (STATE == 1) {
+            DrawSnakeAndFood(" ");
+            switch (MOVING) {
+                case 'A':
+                    MoveLeft();
+                    break;
+                case 'D':
+                    MoveRight();
+                    break;
+                case 'W':
+                    MoveUp();
+                    break;
+                case 'S':
+                    MoveDown();
+                    break;
+            }
+            //DrawSnakeAndFood("0");
+            DrawFood("0");
+            DrawSnake("2312020023120194");
+            Sleep(200 / SPEED);
+        }
+        else {
+            ProcessDead();
+        }
+    }
+}
+
+bool isValidKey(int key) {
+    if ((key != CHAR_LOCK) && (key == 'A' || key == 'D' || key == 'W' || key == 'S'))
+        return true;
+    return false;
+}
+
+void setcursor(bool visible, DWORD size) // set bool visible = 0 - invisible, bool visible = 1 - visible
+{
+	if(size == 0)
+	{
+		size = 20;	// default cursor size Changing to numbers from 1 to 20, decreases cursor width
+	}
+	CONSOLE_CURSOR_INFO lpCursor;	
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = size;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&lpCursor);
+}
+
+int main() {
+    int temp;
+    FixConsoleWindow();
+    StartGame();
+    setcursor(0, 0);  //!!!
+    thread t1(ThreadFunc); //Create thread for snake
+    HANDLE handle_t1 = t1.native_handle(); //Take handle of thread
+
+    while (true) {
+        temp = toupper(getch()); // ???
+        if (STATE == 1) {
+            if (temp == 'P') {
+                PauseGame(handle_t1);
+            }
+            else if (temp == 27) { // 27 is ASCII value of 'ESC' key
+                ExitGame(handle_t1);
+                return 0;
+            }
+            else {
+                ResumeThread(handle_t1);
+
+                //Logic of snake's move
+                if (isValidKey(temp)) {
+                    if (temp == 'D')
+                        CHAR_LOCK = 'A';
+                    else if (temp == 'A')
+                        CHAR_LOCK = 'D';
+                    else if (temp == 'W')
+                        CHAR_LOCK = 'S';
+                    else 
+                        CHAR_LOCK = 'W';
+                    MOVING = temp;
+                }
+            }
+        }
+        else {
+            if (temp == 'Y')
+                StartGame();
+            else {
+                ExitGame(handle_t1);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
